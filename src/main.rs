3790273ae8c481f3a87ff677de4151f416b8a5c4
axezhang <zhang.axe@bytedance.com>
2024-09-27 02:33:27 -0400

    feat✨: 增加新反调试函数

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,19 @@
+use std::env::set_var;
+
 pub mod peb;
 #[cfg(test)]
 pub mod tests;
 
 fn main() {
-    peb::peb_being_debugged();
+    #[cfg(debug_assertions)]
+    set_var("RUST_LOG", "debug");
+
+    #[cfg(not(debug_assertions))]
+    set_var("RUST_LOG", "warn");
+
+    env_logger::init();
+
+    let _ = peb::peb_being_debugged();
+    let _ = peb::peb_being_debugged_asm();
+    let _ = peb::peb_nt_global_flag_asm();
 }
--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -7,3 +7,19 @@ pub fn peb_being_debugged_test() {
         false => println!("peb_being_debugged: false"),
     }
 }
+
+#[test]
+pub fn peb_being_debugged_asm_test() {
+    match peb_being_debugged_asm() {
+        true => println!("peb_being_debugged_asm: true"),
+        false => println!("peb_being_debugged_asm: false"),
+    }
+}
+
+#[test]
+pub fn peb_nt_global_flag_asm_test() {
+    match peb_nt_global_flag_asm() {
+        true => println!("peb_being_debugged_asm: true"),
+        false => println!("peb_being_debugged_asm: false"),
+    }
+}
