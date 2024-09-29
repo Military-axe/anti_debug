f6eff89e1b03389a37c072d5cdfeb0ec96f9fe0c
mi1itray.axe <mi1itray.axe@gmail.com>
2024-09-30 00:18:21 +0800

    test✅: 修改测试集

--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -2,24 +2,24 @@ use crate::{breakpoint, peb::*};
 
 #[test]
 pub fn peb_being_debugged_test() {
-    assert_eq!(WinPeb::peb_being_debugged(), true);
+    assert_eq!(WinPeb::peb_being_debugged(), false);
 }
 
 #[test]
 pub fn peb_being_debugged_asm_test() {
-    assert_eq!(WinPeb::peb_being_debugged_asm(), true);
+    assert_eq!(WinPeb::peb_being_debugged_asm(), false);
 }
 
 #[test]
 pub fn peb_nt_global_flag_asm_test() {
-    assert_eq!(WinPeb::peb_nt_global_flag_asm(), true);
+    assert_eq!(WinPeb::peb_nt_global_flag_asm(), false);
 }
 
 #[test]
 pub fn peb_process_heap_asm_test() {
     assert_eq!(
         WinPeb::peb_process_heap_asm().expect("PEB.ProcessHeap value is invalid"),
-        true
+        false
     );
 }
 
@@ -27,7 +27,7 @@ pub fn peb_process_heap_asm_test() {
 pub fn peb_process_heap_test() {
     assert_eq!(
         WinPeb::peb_process_heap().expect("GetProcessHeap error"),
-        true
+        false
     );
 }
 
