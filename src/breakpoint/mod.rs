67f21e9e0ff4a8658aeb731aa94a5ade94a11658
mi1itray.axe <mi1itray.axe@gmail.com>
2024-10-16 18:16:51 +0800

    feat✨: HardwareBreakPoint功能实现

--- src/breakpoint/mod.rs
+++ src/breakpoint/mod.rs
@@ -3,10 +3,7 @@ use anyhow::Result;
 use log::debug;
 use windows::Win32::{
     Foundation::HANDLE,
-    System::{
-        Diagnostics::Debug::{GetThreadContext, CONTEXT},
-        Threading::GetCurrentThread,
-    },
+    System::Diagnostics::Debug::{GetThreadContext, SetThreadContext, CONTEXT},
 };
 
 impl BeingDebug for CONTEXT {
@@ -19,32 +16,63 @@ impl BeingDebug for CONTEXT {
     }
 }
 
-/// 检测当前线程的Context，判断是否被设置硬件断点
-///
-/// # 返回值
-///
-/// - `Err`: GetThreadContext API报错返回
-/// - `Ok(true)`: 设置了硬件断点
-/// - `Ok(false)`: 未设置硬件断点
-///
-/// # 示例
-///
-/// ```ignore
-/// if is_hardware_breakpoint_set().unwarp() {
-///     println!("Set hardware breakpoint");
-/// } else {
-///     println!("Do not set hardware breakpoint");
-/// }
-/// ```
-pub fn is_hardware_breakpoint_set() -> Result<bool> {
-    let mut context: CONTEXT = CONTEXT::default();
-    let thread_hanle: HANDLE = unsafe { GetCurrentThread() };
-    unsafe { GetThreadContext(thread_hanle, &mut context) }?;
+pub struct HardwareBreakPoint {}
 
-    debug!(
-        "Thread Context ==> Dr0: {}; Dr1: {}; Dr2: {}; Dr3: {}",
-        context.Dr0, context.Dr1, context.Dr2, context.Dr3
-    );
+impl HardwareBreakPoint {
+    /// 检测指定线程的Context，判断是否被设置硬件断点
+    ///
+    /// # 参数
+    ///
+    /// - `thread_handle`: 线程句柄
+    ///
+    /// # 返回值
+    ///
+    /// - `Err`: GetThreadContext API报错返回
+    /// - `Ok(true)`: 设置了硬件断点
+    /// - `Ok(false)`: 未设置硬件断点
+    ///
+    /// # 示例
+    ///
+    /// ```ignore
+    /// if is_hardware_breakpoint_set().unwarp() {
+    ///     println!("Set hardware breakpoint");
+    /// } else {
+    ///     println!("Do not set hardware breakpoint");
+    /// }
+    /// ```
+    pub fn is_hardware_breakpoint_set(thread_hanle: HANDLE) -> Result<bool> {
+        let mut context: CONTEXT = CONTEXT::default();
+        unsafe { GetThreadContext(thread_hanle, &mut context) }?;
 
-    Ok(context.is_being_debug())
+        debug!(
+            "Thread Context ==> Dr0: {}; Dr1: {}; Dr2: {}; Dr3: {}",
+            context.Dr0, context.Dr1, context.Dr2, context.Dr3
+        );
+
+        Ok(context.is_being_debug())
+    }
+
+    /// 清除指定线程的所有硬件断点
+    ///
+    /// # 参数
+    ///
+    /// - `thread_handle`: 线程句柄
+    ///
+    /// # 返回值
+    ///
+    /// - `Err`: GetThreadContext/SetThreadContext失败
+    /// - `Ok(())`: 清空硬件断点成功
+    pub fn clean_hardware_breakpoint(thread_hanle: HANDLE) -> Result<()> {
+        let mut context: CONTEXT = CONTEXT::default();
+        unsafe { GetThreadContext(thread_hanle, &mut context) }?;
+
+        context.Dr0 = 0;
+        context.Dr1 = 0;
+        context.Dr2 = 0;
+        context.Dr3 = 0;
+
+        unsafe { SetThreadContext(thread_hanle, &mut context) }?;
+
+        Ok(())
+    }
 }
